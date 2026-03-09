d61c3e947da3121e6947b47756cb4a750c152aff
simneu16 <simonneumeister16@gmail.com>
2026-03-09 11:27:21 +0100

    add top-level await support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
@@ -11,6 +12,9 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    target: "esnext",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
