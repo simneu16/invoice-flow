aec3e356d01aee2f7131809c3b831b0c725425e2
simneu16 <simonneumeister16@gmail.com>
2026-03-09 11:37:27 +0100

    oprava

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  build: {
-    target: "esnext",
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
